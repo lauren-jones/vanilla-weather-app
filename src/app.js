8ce52e00a43f479958c4c3f3c0d5684082b64c83
lauren-jones <105777927+lauren-jones@users.noreply.github.com>
2022-08-06 20:01:50 +0100

    Integrated forecast api call

--- src/app.js
+++ src/app.js
@@ -24,8 +24,6 @@ function formatDate(timestamp) {
 
 // Function to update weather
 function updateWeather(response) {
-  console.log(response.data);
-
   // Update city name
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
@@ -55,9 +53,12 @@ function updateWeather(response) {
   let weatherIcon = document.querySelector("#weather-icon");
   weatherIcon.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   weatherIcon.setAttribute("alt", `${response.data.weather[0].description}`);
+
+  // Pass coordinates to function that will get forecast
+  getForecast(response.data.coord);
 }
 
 // Convert celsius temp to fahrenheit
@@ -92,8 +93,15 @@ function handleSubmit(event) {
   search(`${cityInputElement.value}`);
 }
 
+// Make api call to get forecast
+function getForecast(coordinates) {
+  let apiKey = "22e2130aeacc47e61254fec6ce6af082";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 // Display forecast
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
@@ -104,7 +112,7 @@ function displayForecast() {
           <div class="col">
             <div class="weather-forecast-date">${day}</div>
             <img
-              src="http://openweathermap.org/img/wn/01d@2x.png"
+              src="https://openweathermap.org/img/wn/01d@2x.png"
               alt=""
               width="60"
             />
